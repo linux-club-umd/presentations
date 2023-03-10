cefc1162936d544f69119890291a7bf3359b8b40
Jeffrey Fisher <jeffrey@jeffreyfisher.net>
2023-03-10 17:34:39 -0500

    Add CUA to config

--- emacs/init.el
+++ emacs/init.el
@@ -12,3 +12,15 @@
 (require 'use-package)
 ;; Download and install configured packages if they aren't already installed.
 (setq use-package-always-ensure t)
+
+(use-package ef-themes)
+;; A nice dark theme. `'modus-operandi` is the light theme version.
+;; You can change the theme with `M-x load-theme`.
+;; You can pick from the ef-themes with `M-x ef-themes-select`.
+(load-theme 'modus-vivendi)
+
+(use-package cua-base
+  :custom
+  (cua-keep-region-after-copy t)
+  :init
+  (cua-mode))
