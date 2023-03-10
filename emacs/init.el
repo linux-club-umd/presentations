b42983fb59b1a452cc9f60d67acbcfc0dcceecf9
Jeffrey Fisher <jeffrey@jeffreyfisher.net>
2023-03-09 22:40:27 -0500

    Create literate config file.

---
+++ emacs/init.el
@@ -0,0 +1,13 @@
+;; Initialize package manager
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+;; Download and install configured packages if they aren't already installed.
+(setq use-package-always-ensure t)
